91c93ece2decd4f506a9a79af298232095a2ef58
Calvin Bascom <calvin.bascom@adtran.com>
2012-06-15 15:05:55 -0400

    Escape the _ and % chars when performing searches

--- lib/data_tables_controller.rb
+++ lib/data_tables_controller.rb
@@ -23,21 +23,21 @@ module DataTablesController
       # override columns
       columns = options_to_columns(options) if options[:columns]
 
-      conditions = options[:conditions] || []
-
-      scope = options[:scope] || :domain
-      named_scope = options[:named_scope]
-      except = options[:except]
-
       # define columns so they are accessible from the helper
       define_columns(modelCls, columns, action)
 
       # define method that returns the data for the table
-      define_datatables_action(self, action, modelCls, scope, conditions, columns, named_scope, except)
+      define_datatables_action(self, action, modelCls, columns, options)
     end
 
-    #named_scope is a combination table that include everything shows in UI, except is the codition used for Ohm's except method, it should be key-value format, such as [['name', 'bluesocket'],['id','1']].  
-    def define_datatables_action(controller, action, modelCls, scope, conditions, columns, named_scope, except)
+    # named_scope is a combination table that include everything shown in UI.
+    # except is the codition used for Ohm's except method, it should be key-value format,
+    # such as [['name', 'bluesocket'],['id','1']].
+    def define_datatables_action(controller, action, modelCls, columns, options = {})
+      conditions = options[:conditions] || []
+      scope = options[:scope] || :domain
+      named_scope = options[:named_scope]
+      except = options[:except]
 
       if modelCls < Ohm::Model
         define_method action.to_sym do
@@ -78,7 +78,7 @@ module DataTablesController
             :sEcho => params[:sEcho].to_i}.to_json
         end
       else
-        #add_search_option will determine whether the search text if empty or not
+        # add_search_option will determine whether the search text is empty or not
         init_conditions = conditions.clone
         add_search_option = false
         define_method action.to_sym do
@@ -86,14 +86,15 @@ module DataTablesController
           unless params[:sSearch].blank?
             search_conditions = []
             columns.find_all { |col| col.has_key?(:attribute) }.each do |col|
-              search_conditions << "(text(#{col[:attribute]}) ILIKE '%%#{params[:sSearch]}%%')"
+              condstr = params[:sSearch].gsub(/_/, '\\\\_').gsub(/%/, '\\\\%')
+              search_conditions << "(text(#{col[:attribute]}) ILIKE '#{condstr}%')"
             end
-            condition_local = '(' + search_conditions.join(" OR ") + ')' 
-            #conditions << '(' + search_conditions.join(" OR ") + ')'
+            condition_local = '(' + search_conditions.join(" OR ") + ')'
           end
 
-          #we just need one conditions string for search at a time. every time we input something else in the search bar we will pop the previous search condition string and push the new string.
-          #conditions << condition_local
+          # We just need one conditions string for search at a time.  Every time we input
+          # something else in the search bar we will pop the previous search condition
+          # string and push the new string.
           if condition_local != ''
             if add_search_option == false
               conditions << condition_local
@@ -104,7 +105,7 @@ module DataTablesController
                 conditions << condition_local
               end
             end
-          else 
+          else
             if add_search_option == true
               if conditions != []
                 conditions.pop
@@ -112,7 +113,7 @@ module DataTablesController
               end
             end
           end
-          
+
           if named_scope
             total_records = modelCls.send(named_scope).count :conditions => init_conditions.join(" AND ")
             total_display_records = modelCls.send(named_scope).count :conditions => conditions.join(" AND ")
